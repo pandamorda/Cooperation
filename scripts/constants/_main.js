57c11e759ef59c2a63f034ffb3f6c9e3d1793891
Maksym Shapovalov <maks0681912507@gmail.com>
2022-08-28 00:30:53 +0300

    Fix(start): optimize html/css/js
    - Restore header on page

--- scripts/constants/_main.js
+++ scripts/constants/_main.js
@@ -1,6 +1,39 @@
 const PROGRESSBAR_PRECISION = 2;
 
+const getElemById = (id) => document.getElementById(id);
+const toggleCNs = (elem, ...cns) => cns.forEach(cn => elem.classList.toggle(cn));
+const getCssVar = (varName) => getComputedStyle(document.body).getPropertyValue(varName);
+
 const PAGE_URLS = {
     console: '/pages/console/',
     tests: '/pages/tests/'
+}
+
+const ID = {
+    html: 'html',
+    header: {
+        profilePart: 'profile-part',
+        btnArrow: 'btn-arrow',
+        btnProfileMain: 'btn-profile-main',
+        btnProfile: 'btn-profile',
+        btnExit: 'btn-exit'
+    }
+}
+
+const CN = {
+    hidden: 'hidden',
+    header: {
+        profileBtnsExpanded: 'profile-btns-expanded',
+        btnAppear: 'btn-appear',
+        btnDisappear: 'btn-disappear'
+    },
+    start: {
+        card: 'card',
+        appearCardL: 'appear-card-l',
+        appearCardR: 'appear-card-r'
+    }
+}
+const CSS_VAR = {
+    btnAnimationTime: '--btn-animation-time',
+    cardAnimationTime: '--card-animation-time'
 }
