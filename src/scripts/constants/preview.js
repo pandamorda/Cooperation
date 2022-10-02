565324b1c99771a5f5c96bf5719186135eb6d5dd
Maksym Shapovalov <maks0681912507@gmail.com>
2022-10-02 20:27:51 +0300

    Fix(preview): reorganize project srtucture
    - fix logo

---
+++ src/scripts/constants/preview.js
@@ -0,0 +1,18 @@
+export const IS_USER_LOGGED = false;
+export const USER_AUTH_MAIN_BTN_TEXT = 'Nickname';
+export const USER_UNAUTH_MAIN_BTN_TEXT = 'Profile';
+
+export const ID = {
+    mainBtn: 'main-btn',
+    loginBtn: 'login-btn',
+    signBtn: 'sign-btn',
+    exitBtn: 'exit-btn',
+    profileBtn: 'profile-btn',
+    crossBtn: 'cross'
+};
+
+export const CN = {
+    hidden: 'hidden',
+    userAuth: 'user-auth',
+    userUnauth: 'user-unauth'
+};
